7a9eab67f8b60f4e00092ab69e1bdb4af32c0864
Marcelo Spacca Barros Ferreiro <marceloferreiro05@gmail.com>
2024-07-02 07:58:13 -0300

    Update MathExamples.java

--- calculos/src/main/java/testando/br/calculos/MathExamples.java
+++ calculos/src/main/java/testando/br/calculos/MathExamples.java
@@ -31,80 +31,60 @@ public class MathExamples
     	System.out.println("The absolute value of " + dblNumC + " = " + dFormat.format(absoluteVal(dblNumC)));
     	System.out.println("The absolute value of " + dblNumG + " = " + dFormat.format(absoluteVal(dblNumG)));
     }
+    
     public static int add(int intNum1, int intNum2) {
-
     	return intNum1 + intNum2;
-
     	}
+    
     public static int subtract(int intNum1, int intNum2) {
-
     	return intNum1 - intNum2;
-
     	}
+    
     public static int multiply(int intNum1, int intNum2) {
-
     	return intNum1 * intNum2;
-
     	}
+    
     public static int divide(int intDividend, int intDivisor) {
-
     	return intDividend / intDivisor;
-
     	}
+    
     public static int modulo(int intDividend, int intDivisor) {
-
     	return intDividend % intDivisor;
-
     	}
+    
     public static int exponent(int base, int power) {
     	return (int) Math.pow(base, power);
-
     	}
     
     public static BigDecimal add(double dblNum1, double dblNum2) {
         return BigDecimal.valueOf(dblNum1).add(BigDecimal.valueOf(dblNum2));
-        
-    }
+        }
 
     public static double subtract(double dblNum1, double dblNum2) {
-
     	return dblNum1 - dblNum2;
-
     	}
 
     public static double multiply(double dblNum1, double dblNum2) {
-
     	return dblNum1 * dblNum2;
-
     	}
 
     public static double divide(double dblNum1, double dblNum2) {
-
     	return dblNum1 / dblNum2;
-
     	}
     
     public static double exponent(double base, double power) {
-
     	return Math.pow(base, power);
-
     	}
 
     public static double squareRoot(double number) {
-
     	return Math.sqrt(number);
-
     	}
 
     public static double cubeRoot(double number) {
-
     	return Math.cbrt(number);
-
     	}
 
     public static double absoluteVal(double number) {
-
     	return Math.abs(number);
-
     	}
     }
